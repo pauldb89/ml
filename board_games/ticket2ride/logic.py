95508d710a93f7befc5e8cddf9e3a8460f46d33c
Paul Baltescu <pauldb@openai.com>
2025-01-14 15:36:58 -0800

    Implement game logic.

--- board_games/ticket2ride/logic.py
+++ board_games/ticket2ride/logic.py
@@ -2,12 +2,11 @@ import abc
 import copy
 import itertools
 import random
-from dataclasses import dataclass, Field, field
-from typing import Self
+from dataclasses import dataclass
 
 from board_games.ticket2ride.consts import NUM_LAST_TURN_CARS, NUM_INITIAL_PLAYER_CARDS, ANY, \
     ROUTES, NUM_VISIBLE_CARDS, NUM_INITIAL_TRAIN_CARS, COLORS, NUM_COLOR_CARDS, NUM_ANY_CARDS, \
-    TICKETS
+    TICKETS, MAX_VISIBLE_ANY_CARDS
 from board_games.ticket2ride.data_models import Card, Ticket, Color, ActionType
 from board_games.ticket2ride.disjoint_sets import DisjointSets
 
@@ -36,66 +35,71 @@ class TicketDeck:
 
 
 @dataclass
-class DiscardPile:
-    cards: list[Card] = field(default_factory=list)
+class CardDeck:
+    deck: list[Card]
+    discard_pile: list[Card]
 
-    def add(self, card: Card) -> None:
-        self.cards.append(card)
-
-    def __len__(self) -> int:
-        return len(self.cards)
-
-    def reset(self) -> None:
-        self.cards = []
-
-
-@dataclass
-class Deck:
-    cards: list[Card]
-
-    @classmethod
-    def init(cls) -> Self:
-        cards = [Card(color=ANY) for _ in range(NUM_ANY_CARDS)]
+    def __init__(self) -> None:
+        self.discard_pile = []
+        self.deck = [Card(color=ANY) for _ in range(NUM_ANY_CARDS)]
         for color in COLORS:
             for _ in range(NUM_COLOR_CARDS):
-                cards.append(Card(color=color))
+                self.deck.append(Card(color=color))
 
-        random.shuffle(cards)
-        return cls(cards=cards)
+        random.shuffle(self.deck)
 
-    @classmethod
-    def from_discard_pile(cls, pile: DiscardPile) -> Self:
-        cards = pile.cards
-        random.shuffle(cards)
-        pile.reset()
-        return cls(cards=cards)
+    def draw(self) -> Card:
+        if len(self.deck) == 0:
+            self.deck = self.discard_pile
+            self.discard_pile = []
+            random.shuffle(self.deck)
+
+        assert len(self.deck) > 0
+        return self.deck.pop()
+
+    def discard(self, card: Card) -> None:
+        self.discard_pile.append(card)
 
     def __len__(self) -> int:
-        return len(self.cards)
-
-    def get(self) -> Card:
-        assert len(self.cards) > 0
-        return self.cards.pop()
+        return len(self.deck) + len(self.discard_pile)
 
 
 class Board:
     route_ownership: dict[int, RouteInfo]
     ticket_deck: TicketDeck
-    card_deck: Deck
-    discard_pile: DiscardPile
-    visible_cards: list[Card]
+    card_deck: CardDeck
     train_cars: list[int]
+    route_points: list[int]
+    visible_cards: list[Card]
 
     # TODO(pauldb): Keep track of visible points.
     def __init__(self, num_players: int) -> None:
         self.route_ownership = {}
         self.ticket_deck = TicketDeck()
-        self.card_deck = Deck.init()
-        self.discard_pile = DiscardPile()
+        self.card_deck = CardDeck()
 
-        # TODO(pauldb): Handle edge case where the initial cards have 3 or more locomotive cards.
-        self.visible_cards = [self.card_deck.get() for _ in range(NUM_VISIBLE_CARDS)]
         self.train_cars = [NUM_INITIAL_TRAIN_CARS for _ in range(num_players)]
+        self.route_points = [0 for _ in range(num_players)]
+
+        self.visible_cards = []
+        self.reveal_cards()
+
+    def reveal_cards(self) -> None:
+        while True:
+            while len(self.visible_cards) < NUM_VISIBLE_CARDS:
+                self.visible_cards.append(self.card_deck.draw())
+
+            num_visible_any_cards = 0
+            for card in self.visible_cards:
+                if card.color == ANY:
+                    num_visible_any_cards += 1
+
+            if num_visible_any_cards > MAX_VISIBLE_ANY_CARDS:
+                for card in self.visible_cards:
+                    self.card_deck.discard(card)
+                self.visible_cards = []
+            else:
+                break
 
 
 # TODO(pauldb): Maybe rename to PlayerData or PlayerInfo?
@@ -175,7 +179,7 @@ def count_ticket_points(board: Board, player: Player) -> int:
 
 def get_valid_actions(board: Board, player: Player) -> list[ActionType]:
     valid_action_types = []
-    if len(board.card_deck) + len(board.discard_pile) >= 2:
+    if len(board.card_deck) >= 2:
         valid_action_types.append(ActionType.DRAW_CARDS)
 
     if len(board.ticket_deck) >= 3:
@@ -261,7 +265,7 @@ class Game:
         for player_id, policy in enumerate(policies):
             player = Player(player_id=player_id)
             for _ in range(NUM_INITIAL_PLAYER_CARDS):
-                card = self.board.card_deck.get()
+                card = self.board.card_deck.draw()
                 player.card_counts[card.color] += 1
                 self.players.append(player)
 
@@ -281,7 +285,19 @@ class Game:
 
             action_type = policy.choose_action(self.board, player)
             if action_type == ActionType.DRAW_CARDS:
-                pass
+                for can_draw_any in [True, False]:
+                    card = policy.draw_card(self.board, player, can_draw_any=can_draw_any)
+
+                    if card is None:
+                        card = self.board.card_deck.draw()
+                        player.card_counts[card.color] += 1
+                    else:
+                        self.board.visible_cards.remove(card)
+                        player.card_counts[card.color] += 1
+                        self.board.reveal_cards()
+
+                        if card.color == ANY:
+                            break
             elif action_type == ActionType.DRAW_TICKETS:
                 tickets = self.board.ticket_deck.get()
                 selected_tickets = policy.choose_tickets(
@@ -292,7 +308,25 @@ class Game:
                 )
                 player.tickets.extend(selected_tickets)
             elif action_type == ActionType.BUILD_ROUTE:
-                pass
+                route_info = policy.build_route(self.board, player)
+
+                assert route_info.route_id not in self.board.route_ownership
+                self.board.route_ownership[route_info.route_id] = route_info
+
+                route = ROUTES[route_info.route_id]
+                num_regular_cards = route.length - route_info.num_any_cards
+                for _ in range(num_regular_cards):
+                    player.card_counts[route_info.color] -= 1
+                    self.board.card_deck.discard(Card(color=route_info.color))
+
+                for _ in range(route_info.num_any_cards):
+                    player.card_counts[ANY] -= 1
+                    self.board.card_deck.discard(Card(color=ANY))
+
+                assert self.board.train_cars[player.id] >= route.length
+                self.board.train_cars[player.id] -= route.length
+
+                self.board.route_points[player.id] += route.value
             else:
                 raise ValueError(f"Unknown action type: {action_type}")
 
